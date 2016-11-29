bdd251d0f3901c7db10f83faf22643aaa372fe27
Alvaro Dasso <alvaro.dasso@outlook.com>
2016-11-29 11:47:09 -0700

    Before work in policy Home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #!--- Added by AlvaroD ----#
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
 end

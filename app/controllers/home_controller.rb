c4511969cd3daa0fbb388bf7a52cbda59a1d342d
nuzen <venkatesh.jatla@gmail.com>
2016-11-28 17:05:18 -0700

    Added twilio, Home page has form to send SMS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+    def send_sms
+    message = params[:message]
+    number = params[:number]
+    account_sid = 'AC2ae9c3f43a9305ce257c141915434eb8'
+    auth_token = '91c25d476b725885aa4ac8d138d24e0e'
+    
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    
+    @message = @client.account.messages.create({:to => "+1"+"#{number}",
+                                                :from => "+15057388535",
+                                                :body => "#{message}"})
+    redirect_to '/'
+  end
   def index
   end
 end

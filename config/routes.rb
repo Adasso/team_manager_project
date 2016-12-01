36581bc0708b88234f06c2c612c478a791ec4dbd
jtabar01 <jtabar01@unm.edu>
2016-12-01 15:59:23 -0700

    fixed conflict with jtabar01 and alex app versions

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   post '/send_sms' => 'home#send_sms'
 
   get 'messages', to: 'home#messages'
-  get 'schedule', to: 'home#schedule'
+  get 'schedules', to: 'home#schedules'
   get 'statistics', to: 'home#statistics'
 
 end

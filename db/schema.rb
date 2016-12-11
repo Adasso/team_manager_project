5cb79a4f2c9c77cbab8c7a10fd27c70a3eee5910
Alex Hidalgo <ahidal01@unm.edu>
2016-12-11 15:18:12 -0700

    Fized data base error

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161207023258) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -35,8 +32,6 @@ ActiveRecord::Schema.define(version: 20161207023258) do
     t.integer  "resource_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-    t.index ["name"], name: "index_roles_on_name", using: :btree
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
   end
@@ -53,7 +48,6 @@ ActiveRecord::Schema.define(version: 20161207023258) do
     t.integer  "group_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.index ["group_id"], name: "index_teamstats_on_group_id", using: :btree
     t.index ["group_id"], name: "index_teamstats_on_group_id"
   end
 
@@ -78,9 +72,6 @@ ActiveRecord::Schema.define(version: 20161207023258) do
     t.integer  "height_in_inches"
     t.integer  "weight_in_pounds"
     t.integer  "group_id"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["group_id"], name: "index_users_on_group_id", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["group_id"], name: "index_users_on_group_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -89,9 +80,7 @@ ActiveRecord::Schema.define(version: 20161207023258) do
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
   end
-
-  add_foreign_key "users", "groups"
 
 end

d29646073512354ac06547e982537ae0f451a0ae
Alvaro Dasso <alvaro.dasso@outlook.com>
2016-12-12 13:24:11 -0700

    TeamStat Policy. Coach and admin can create and edit. However, only admin can delete statitics.

--- app/policies/teamstat_policy.rb
+++ app/policies/teamstat_policy.rb
@@ -4,4 +4,24 @@ class TeamstatPolicy < ApplicationPolicy
       scope
     end
   end
+
+  def new?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def edit?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def create?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def update?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def destroy?
+    user.has_role? :admin
+  end
 end

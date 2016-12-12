f59f626bc5d7b6cfecf0c363b4430a1dbe0c20a4
Alvaro Dasso <alvaro.dasso@outlook.com>
2016-12-12 13:22:58 -0700

    Meeting Policy. Coach and admin can create, edit or delete

--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -4,4 +4,25 @@ class MeetingPolicy < ApplicationPolicy
       scope
     end
   end
+
+  def new?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def edit?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def create?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def update?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
+  def destroy?
+    user.has_role? :admin or user.has_role? :coach
+  end
+
 end
